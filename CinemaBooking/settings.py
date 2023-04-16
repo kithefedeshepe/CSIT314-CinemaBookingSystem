5a73cd4cc8b7ef9a80ece99cfc872d1616c31ce0
Agu <gkn060702@gmail.com>
2023-04-16 17:46:03 +0800

    Allow localhost

--- CinemaBooking/settings.py
+++ CinemaBooking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s=+fqa=i1(k=$parisvz-x9%9h7ub&)#_v*d(ov@lth2p@^7j!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
