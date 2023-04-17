3a7253702bb497ca9b45be3bb7a83e5f938ba0fe
mikyung <mikyung.jin.94@gmail.com>
2023-04-17 22:29:11 +0800

    'testing'

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('email', 'role')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role'),
+        }),
+    )
+    list_display = ('username', 'email', 'role', 'is_staff')
+    list_filter = ('role',)
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+
+admin.site.register(User, CustomUserAdmin)
