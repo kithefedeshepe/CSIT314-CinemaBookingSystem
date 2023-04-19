96965dedac6174b06aa41ab010be75fae74d5703
Agu <gkn060702@gmail.com>
2023-04-19 19:54:14 +0800

    update API

--- api/views.py
+++ api/views.py
@@ -5,16 +5,16 @@ from core.models import User
 from .serializers import UserSerializer
 # Create your views here.
 
-@api_view(['GET'])
-def getUserAccount(request):
-    user_account = User.objects.all()
-    serializer = UserSerializer(user_account, many = True)
-    return Response(serializer.data)
+class AccountController:
+    @api_view(['GET'])
+    def getUserAccount(request):
+        user_account = User.objects.all()
+        serializer = UserSerializer(user_account, many = True)
+        return Response(serializer.data)
 
-@api_view(['POST'])
-def addUserAccount(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.isvalid(raise_execption=True):
-        serializer.save()
-        return Response()
-    return Response({"invalid":"bad entry"}, status=400)
+    @api_view(['POST'])
+    def addUserAccount(request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
