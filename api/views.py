33dc091d09e3f4df12907a9e40d43143cf49c531
Agu <gkn060702@gmail.com>
2023-04-19 22:53:11 +0800

    update API

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.hashers import make_password
+from django.db import DatabaseError
 from core.models import User
 from .serializers import UserSerializer
 # Create your views here.
@@ -15,7 +16,11 @@ class AccountController:
 
     @api_view(['POST'])
     def RegisterAccount(request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
+        try:
+            serializer = UserSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+            return Response({"invalid": "bad data"}, status = 400)
+        except DatabaseError as e:
+            return Response({"error": "Bad data"}, status=500)
