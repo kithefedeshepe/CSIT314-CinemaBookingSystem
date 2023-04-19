1287183402f244a4d265b6212ac4789e36ba515f
mikyung <mikyung.jin.94@gmail.com>
2023-04-19 22:48:48 +0800

    DB error fix test4

--- core/admin.py
+++ core/admin.py
@@ -4,5 +4,8 @@ from .models import User, UserProfile
 class UserProfileAdmin(admin.ModelAdmin):
     pass
 
+    class Meta:
+        model = UserProfile
+
 admin.site.register(User)
 admin.site.register(UserProfile, UserProfileAdmin)
--- core/models.py
+++ core/models.py
@@ -33,3 +33,6 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.username
+    
+    class Meta:
+        db_table = 'profiles'
