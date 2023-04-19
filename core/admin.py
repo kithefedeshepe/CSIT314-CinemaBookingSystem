0de83a98707571fe97d2d9645ef1cf93538a7c23
mikyung <mikyung.jin.94@gmail.com>
2023-04-19 22:00:42 +0800

    DB error fix test3

--- core/admin.py
+++ core/admin.py
@@ -1,72 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext_lazy as _
-from .models import User, Profile, Movie, Image
-from django.utils.html import format_html
+from .models import User, UserProfile
 
-class CustomUserAdmin(UserAdmin):
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('email', 'role')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        (_('Important dates'), {'fields': ('last_login',)}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'role'),
-        }),
-    )
-    list_display = ('username', 'email', 'role', 'is_staff')
-    list_filter = ('role',)
-    search_fields = ('username', 'email')
-    ordering = ('username',)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
 
-    def has_module_permission(self, request):
-        if request.user.is_superuser:
-            return True
-        else:
-            if not request.user.is_anonymous:
-                if request.user.role == 'UserAdmin':
-                    return True
-        return False
-
-class ProfileAdmin(admin.ModelAdmin):
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'role'),
-        }),
-    )
-       
-    list_display = ('user', 'name', 'date_of_birth', 'loyalty_points')
-    list_filter = ('user__role',)
-    search_fields = ('user__username', 'name')
-    ordering = ('user__username',)
-
-    def has_module_permission(self, request):
-        if request.user.is_superuser:
-            return True
-        else:
-            if not request.user.is_anonymous:
-                if request.user.role == 'CinemaManager':
-                    return False
-                if request.user.role == 'UserAdmin':
-                    return True
-        return False
-
-class ImageInline(admin.TabularInline):
-    model = Image
-
-class MovieAdmin(admin.ModelAdmin):
-    inlines = [ImageInline]
-    list_display = ('image_tag', 'movie_title', 'movie_description')
-
-    def image_tag(self, obj):
-        return format_html('<img src="{}" height="50"/>'.format(obj.movie_images.first().image.url))
-
-    image_tag.short_description = 'Image'
-    
-admin.site.register(User, CustomUserAdmin)
-admin.site.register(Profile, ProfileAdmin)
-admin.site.register(Movie, MovieAdmin)
+admin.site.register(User)
+admin.site.register(UserProfile, UserProfileAdmin)
