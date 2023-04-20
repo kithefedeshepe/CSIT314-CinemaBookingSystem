30aa6323fde4fd000ace6b7c1b000ac78ac7cdf6
Agu <gkn060702@gmail.com>
2023-04-20 22:50:32 +0800

    update cors

--- CinemaBooking/settings.py
+++ CinemaBooking/settings.py
@@ -61,7 +61,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
 
 ROOT_URLCONF = 'CinemaBooking.urls'
 
