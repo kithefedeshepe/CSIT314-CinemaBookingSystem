833070ba731fd7f8582f35b318d37b3f14919c16
Agu <gkn060702@gmail.com>
2023-04-22 21:47:48 +0800

    turn debug on

--- CinemaBooking/settings.py
+++ CinemaBooking/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s=+fqa=i1(k=$parisvz-x9%9h7ub&)#_v*d(ov@lth2p@^7j!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
