53e8e5e2875dbd8085269177e0952a46c766a8fc
Jpark482 <nolgoitsu@gmail.com>
2023-04-24 20:13:40 +0800

    3nd trial

--- unit_test.py
+++ unit_test.py
@@ -4,16 +4,20 @@ from .models import User
 
 class TestLogin(TestCase):
     def test_login_success(self):
+        media_type = "test_media_type"
         username = "test_user"
         password = "test_password"
+        email - "test_email"
         
         response = requests.post("https://csit-314-cinema-booking-system.vercel.app/login", json={"username": username, "password": password})
         
         self.assertEqual(response.status_code, 200)
 
     def test_login_failure(self):
+        media_type = "test_media_type"
         username = "test_user"
         password = "wrong_password"
+        email - "test_email"
         
         response = requests.post("https://csit-314-cinema-booking-system.vercel.app/login", json={"username": username, "password": password})
  
