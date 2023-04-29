da348d189e69ed7c47ef2fae592cf8d1b87c3ab7
Agu <gkn060702@gmail.com>
2023-04-30 02:56:23 +0800

    update unit test

--- UnitTest/test_mov_del.py
+++ UnitTest/test_mov_del.py
@@ -1,12 +1,12 @@
-from core.models import User, MovieImage
+from core.models import User, Movie
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from api.checklist import *
-import base64
 from django.urls import reverse
+from datetime import timedelta, date
 
-class MovieImageAddTestCase(APITestCase):
+class TestMovDel(APITestCase):
     def setUp(self):
         self.client = APIClient()
         # create an admin user
@@ -16,27 +16,28 @@ class MovieImageAddTestCase(APITestCase):
         response = self.client.post('/login/', {'username': 'admin', 'password': 'password'})
         self.admin_token = response.data['token']
         self.client.cookies['token'] = self.admin_token
-        if add_mov_img:
-            self.url = reverse('addImg')
-        with open('UnitTest/testimg.png', 'rb') as f:
-            img_data = f.read()
-        self.base64_img_data = base64.b64encode(img_data).decode('utf-8')
+        if del_mov:
+            self.url = reverse('delMov')
+        #setup movie object
+        self.movie_obj = Movie.objects.create(id = 0, movie_title='test', duration=timedelta(hours=1, minutes=30), release_date=date(2022, 5, 1), cast='John Doe',director='Jane Smith',movie_description='A test movie')
 
 
-    def test_add_movimg(self):
-        if not add_mov_img:
+    def test_del_mov(self):
+        if not del_mov:
             return
+        
+        self.movie_id = self.movie_obj.id;
         payload = {
-            'movie_id': '0',
-            'img_data': self.base64_img_data
+            'id' : self.movie_id,
         }
 
         response = self.client.post(self.url, payload)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print("\nUnit test addImg_1 passed")
+        self.assertFalse(Movie.objects.filter(id=self.movie_id).exists())
+        print("\nUnit test delMov_1 passed")
 
-    def test_add_movimg_no_permission(self):
-        if not add_mov_img:
+    def test_delete_mov_no_permission(self):
+        if not del_mov:
             return
         self.usertest = User.objects.create_user(username='testuser', password='password', email='testusr@gmail.com', role='Customer')
         self.usertest.save()
@@ -45,38 +46,23 @@ class MovieImageAddTestCase(APITestCase):
         self.test_token = response.data['token']
         self.client.cookies['token'] = self.test_token
 
+        self.movie_id = self.movie_obj.id;
         payload = {
-            'movie_id': '0',
-            'img_data': self.base64_img_data
+            'id' : self.movie_id,
         }
 
         response = self.client.post(self.url, payload)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        print("\nUnit test addImg_2 passed")
-
-
+        print("\nUnit test delMov_2 passed")
     
-    def test_add_movimg_id_notfound(self):
-        if not add_mov_img:
+    def test_del_mov_invalid(self):
+        if not update_mov:
             return
         
         payload = {
-            'movie_id': '190234781239412736412',
-            'img_data': self.base64_img_data
-        }
-
-        response = self.client.post(self.url, payload)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        print("\nUnit test addImg_3 passed")
-    
-    def test_add_movimg_invalid(self):
-        if not add_mov_img:
-            return
-        
-        payload = {
-            'img_data': self.base64_img_data
+            'id': 'asdhfgasdhf71346715234thisdontmakeanysense',
         }
 
         response = self.client.post(self.url, payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        print("\nUnit test addImg_4 passed")
+        print("\nUnit test delMov_3 passed")
--- UnitTest/test_mov_update.py
+++ UnitTest/test_mov_update.py
@@ -1,12 +1,12 @@
-from core.models import User, MovieImage
+from core.models import User, Movie
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from api.checklist import *
-import base64
 from django.urls import reverse
+from datetime import timedelta, date
 
-class MovieImageAddTestCase(APITestCase):
+class TestMovUpdate(APITestCase):
     def setUp(self):
         self.client = APIClient()
         # create an admin user
@@ -16,27 +16,34 @@ class MovieImageAddTestCase(APITestCase):
         response = self.client.post('/login/', {'username': 'admin', 'password': 'password'})
         self.admin_token = response.data['token']
         self.client.cookies['token'] = self.admin_token
-        if add_mov_img:
-            self.url = reverse('addImg')
-        with open('UnitTest/testimg.png', 'rb') as f:
-            img_data = f.read()
-        self.base64_img_data = base64.b64encode(img_data).decode('utf-8')
+        if update_mov:
+            self.url = reverse('updateMov')
+        #setup movie object
+        self.movie_obj = Movie.objects.create(id = 0, movie_title='test', duration=timedelta(hours=1, minutes=30), release_date=date(2022, 5, 1), cast='John Doe',director='Jane Smith',movie_description='A test movie')
 
 
-    def test_add_movimg(self):
-        if not add_mov_img:
+    def test_update_mov(self):
+        if not update_mov:
             return
+        
         payload = {
-            'movie_id': '0',
-            'img_data': self.base64_img_data
+            'id' : '0',
+            'movie_title': 'test123', 
+            'duration' : timedelta(hours=1, minutes=30), 
+            'release_date' :date(2022, 5, 1), 
+            'cast' : 'John Doe Anderson',
+            'director' :'Jane Smith',
+            'movie_description' : 'A test movie'
         }
 
         response = self.client.post(self.url, payload)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print("\nUnit test addImg_1 passed")
+        self.assertEqual(self.movie_obj.movie_title, 'test123')
+        self.assertEqual(self.movie_obj.cast, 'John Doe Anderson')
+        print("\nUnit test updateMov_1 passed")
 
-    def test_add_movimg_no_permission(self):
-        if not add_mov_img:
+    def test_update_mov_no_permission(self):
+        if not update_mov:
             return
         self.usertest = User.objects.create_user(username='testuser', password='password', email='testusr@gmail.com', role='Customer')
         self.usertest.save()
@@ -46,37 +53,27 @@ class MovieImageAddTestCase(APITestCase):
         self.client.cookies['token'] = self.test_token
 
         payload = {
-            'movie_id': '0',
-            'img_data': self.base64_img_data
+            'id' : '0',
+            'movie_title': 'test123', 
+            'duration' : timedelta(hours=1, minutes=30), 
+            'release_date' :date(2022, 5, 1), 
+            'cast' : 'John Doe Anderson',
+            'director' :'Jane Smith',
+            'movie_description' : 'A test movie'
         }
 
         response = self.client.post(self.url, payload)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        print("\nUnit test addImg_2 passed")
-
-
+        print("\nUnit test updateMov_2 passed")
     
-    def test_add_movimg_id_notfound(self):
-        if not add_mov_img:
+    def test_update_mov_invalid(self):
+        if not update_mov:
             return
         
         payload = {
-            'movie_id': '190234781239412736412',
-            'img_data': self.base64_img_data
-        }
-
-        response = self.client.post(self.url, payload)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        print("\nUnit test addImg_3 passed")
-    
-    def test_add_movimg_invalid(self):
-        if not add_mov_img:
-            return
-        
-        payload = {
-            'img_data': self.base64_img_data
+            'id': 'asdhfgasdhf71346715234thisdontmakeanysense',
         }
 
         response = self.client.post(self.url, payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        print("\nUnit test addImg_4 passed")
+        print("\nUnit test updateMov_3 passed")
