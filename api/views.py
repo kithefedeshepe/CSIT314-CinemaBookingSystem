61865c02a617d3cc7f687ccb9524d06f8cbaffc6
bhh97 <hbohao@outlook.com>
2023-05-02 17:53:26 +0800

    Add update movie function

--- api/views.py
+++ api/views.py
@@ -349,6 +349,7 @@ class Movies(APIView):
         # Validate serializer data
         if serializer.is_valid():
             serializer.save()
+            
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             # Return 400 if data is invalid
