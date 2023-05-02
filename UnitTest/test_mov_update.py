8ba67fcbacbfe8927fcf8a73b56c146185a071ab
bhh97 <hbohao@outlook.com>
2023-05-02 22:30:42 +0800

    update movie functions

--- UnitTest/test_mov_update.py
+++ UnitTest/test_mov_update.py
@@ -71,7 +71,7 @@ class TestMovUpdate(APITestCase):
             return
         
         payload = {
-            'id': '0i',
+            'id': ' ',
         }
 
         response = self.client.post(self.url, payload)
