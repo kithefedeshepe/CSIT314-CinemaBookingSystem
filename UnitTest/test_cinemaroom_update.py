7db8de41be2eb1ece7066e9d3cb1a1f1cc90fcd4
Agu <gkn060702@gmail.com>
2023-05-09 01:31:31 +0800

    update

--- UnitTest/test_cinemaroom_update.py
+++ UnitTest/test_cinemaroom_update.py
@@ -34,7 +34,8 @@ class TestUpdateCinemaRoom(APITestCase):
 
         response = self.client.post(self.url, payload)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(self.cr_obj.capacity, 50)
+        obj_result = CinemaRoom.objects.filter(name=self.cr_update_target)
+        self.assertEqual(obj_result.capacity, 50)
         print("\nUnit test updateCR_1 passed")
 
 
